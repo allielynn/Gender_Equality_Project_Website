06875c5da6d1f7d1a474458493b79e942746368b
Allison Lynn <allienlynn@gmail.com>
2025-11-06 14:57:08 -0800

    Deleted steup

--- setup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Install Quarto on Netlify build environment
-
-curl -LO https://quarto.org/download/latest/quarto-linux-amd64.deb
-dpkg -i quarto-linux-amd64.deb || (apt-get update && apt-get install -y ./quarto-linux-amd64.deb)
